0ecee4273a9cc05b9f4a4a3446ce74e261dcb625
Geoff Baker <geoffreybaker_uk@yahoo.co.uk>
2013-08-12 16:51:37 +0100

    Added utility program to generate RSA keys

---
+++ CryptoChat_Server/src/io/github/bakerg/util/RSAKeygen.java
@@ -0,0 +1,64 @@
+package io.github.bakerg.util;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+
+public class RSAKeygen {
+
+	public static void main(String[] args) {
+		KeyPairGenerator kpg = null;
+		KeyFactory fact = null; 
+		try {
+			kpg = KeyPairGenerator.getInstance("RSA");
+			fact = KeyFactory.getInstance("RSA");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		kpg.initialize(2048);
+		KeyPair kp = kpg.genKeyPair();
+		
+		RSAPublicKeySpec pub = null;
+		RSAPrivateKeySpec priv = null;
+		try {
+			pub = fact.getKeySpec(kp.getPublic(), RSAPublicKeySpec.class);
+			priv = fact.getKeySpec(kp.getPrivate(), RSAPrivateKeySpec.class);
+		} catch (InvalidKeySpecException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+
+		try {
+			saveToFile("public.key", pub.getModulus(), pub.getPublicExponent());
+			saveToFile("private.key", priv.getModulus(), priv.getPrivateExponent());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public static void saveToFile(String fileName,
+			  BigInteger mod, BigInteger exp) throws IOException {
+			  ObjectOutputStream oout = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(fileName)));
+			  try {
+			    oout.writeObject(mod);
+			    oout.writeObject(exp);
+			  } catch (Exception e) {
+			    throw new IOException("Unexpected error", e);
+			  } finally {
+			    oout.close();
+			  }
+			}
+
+}
